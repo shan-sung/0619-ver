95c12ebfcef6eb92d0d9e512153ba789e36a0719
Shan <shansung25@gmail.com>
2025-08-04 13:59:34 +0800

    card reorganize

--- app/src/main/java/com/example/myapplication/navigation/subgraph/ExploreNav.kt
+++ app/src/main/java/com/example/myapplication/navigation/subgraph/ExploreNav.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.navigation.subgraph
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
